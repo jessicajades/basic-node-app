96320a0691f49a4404c5059f82961c951f46c958
Jessica Shepherd <jessicashepherd@Jessicas-MacBook-Pro.local>
2019-05-14 11:04:27 -0700

    update

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 const greetings = require('./greeting.js');
+const goodbye = require('./goodbye.js');
 
 greetings.greetingEnglish();
 greetings.greetingJapanese();
 greetings.greetingSpanish();
 greetings.greetingIcelandic();
-
-
-const goodbye = require('./goodbye.js');
 
 goodbye.goodbyeEnglish();
 goodbye.goodbyeSpanish();
